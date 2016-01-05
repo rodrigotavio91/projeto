e83c615e1d7167ecf297133c75d084b7ff364536
Rodrigo Otavio <rodrigotavio91@gmail.com>
2016-01-05 06:50:45 -0200

    correcao no menu ativo por modulo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 module ApplicationHelper
-  def nav_link(link_text, link_path)
-	class_name = current_page?(link_path) ? 'active' : ''
+  def nav_link(link_text, link_path, controller_name)
+	if (controller.controller_name == controller_name)
+	  class_name = 'active'
+	else
+	  class_name = ''
+	end
 
 	content_tag(:li, :class => class_name) do
 	  link_to link_text, link_path
